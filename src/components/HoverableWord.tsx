f295596e3b922508c77dc005816316b9fe76748d
salvadorxe <salv.xe@gmail.com>
2024-07-09 18:40:28 -0400

    Render fixes

--- src/components/HoverableWord.tsx
+++ src/components/HoverableWord.tsx
@@ -8,16 +8,26 @@ interface HoverProps {
 
 function HoverableWord(props: HoverProps) {
   return (
-    <motion.span
-      whileHover={{ color: props.hoverColor }}
-      transition={{ type: "easeInOut", duration: 0.2 }}
+    <motion.div
+      transition={{ type: "easeInOut", duration: 0.5 }}
       style={{
-        cursor: "pointer",
-        color: props.color,
+        display: "inline-block",
+        position: "relative",
       }}
     >
-      {props.text}
-    </motion.span>
+      <motion.span
+        whileHover={{ color: props.hoverColor }}
+        transition={{ type: "easeInOut", duration: 0.2 }}
+        style={{
+          cursor: "pointer",
+          color: props.color,
+          zIndex: 1,
+          position: "relative",
+        }}
+      >
+        <span className="shimmer">{props.text}</span>
+      </motion.span>
+    </motion.div>
   );
 }
 
